6da0b318259a1cf617dc22a96723df3a1ad55fe6
Samuel Cline <samuellcline@gmail.com>
2019-03-09 10:45:26 -0700

    Added Validation and changed the forms have drop down menus instead of text entry for preset options

--- TeamMegaDeskWeb/TeamMegaDeskWeb/Models/DeskQuote.cs
+++ TeamMegaDeskWeb/TeamMegaDeskWeb/Models/DeskQuote.cs
@@ -10,18 +10,25 @@ namespace TeamMegaDeskWeb.Models
     {
         public int ID { get; set; }
         [Display(Name = "Name")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"), Required, StringLength(50)]
         public string CustomerName { get; set; }
 
         [DataType(DataType.Date)]
         [Display(Name = "Date")]
+        [Required]
         public DateTime QuoteDate { get; set; }
+        [Range(24, 96),Required]
         public int Width { get; set; }
+        [Range(12, 48), Required]
         public int Depth { get; set; }
+        [Range(0, 7), Required]
         public int Drawers { get; set; }
         public string Material { get; set; }
         [Display(Name = "Days")]
+        [Required]
         public int RushDays { get; set; }
         [Display(Name = "Total")]
+        [Required]
         public decimal QuoteAmount { get; set; }
     }
 }
